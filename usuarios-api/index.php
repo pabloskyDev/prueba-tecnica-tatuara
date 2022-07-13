e5da1c8741c82ebe81fe1e94efaa526a36b6a334
Juan Pablo Betancur <jpablobetancur.master2000@gmail.com>
2022-07-13 13:59:23 -0500

    Agregar ejemplo Ajax

---
+++ usuarios-api/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Usuarios-api</title>
+</head>
+<body>
+    <h1>Petición Ajax</h1>
+    <h3>A Simple AJAX Example <span>with</span> Plain JavaScript</h3>
+
+    <p>Data retrieved from <a href="https://en.wikipedia.org/wiki/Albert_Einstein" target="_blank">Wikipedia</a></p>
+    <div>
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/Einstein.jpg" alt="Einstein">
+        <button id="request">Learn more about Einstein</button>
+        <div id="bio"></div>
+    </div>
+
+    <script src="app.js"></script>
+</body>
+</html>
