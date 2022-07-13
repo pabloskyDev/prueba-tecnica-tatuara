aeefcd7fed89e683909de434106bbb0d2195c405
Juan Pablo Betancur <jpablobetancur.master2000@gmail.com>
2022-07-13 11:13:04 -0500

    Creación y pruebas de API REST con PHP

--- funciones.php
+++ funciones.php
@@ -17,4 +17,46 @@ function csrf() {
     }
 }
 
+// Funciones creadas para crear una API con todos sus métodos
+
+//Abrir conexion a la base de datos
+function connect($db)
+{
+    try {
+        $conn = new PDO(
+            "mysql:host={$db['host']};
+            dbname={$db['name']};
+            charset=utf8", $db['user'], $db['pass']);
+        // $conn = new PDO("mysql:host=localhost;dbname=tuatara_test", "root", "root");
+        // error_log(print_r($db, true));
+
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    } catch (PDOException $exception) {
+        exit($exception->getMessage());
+    }
+}
+
+//Obtener parametros para updates
+function getParams($input)
+{
+   $filterParams = [];
+   foreach($input as $param => $value)
+   {
+        $filterParams[] = "$param=:$param";
+   }
+   return implode(", ", $filterParams);
+}
+
+//Asociar todos los parametros a un sql
+function bindAllValues($statement, $params)
+{
+    foreach($params as $param => $value)
+    {
+        $statement->bindValue(':'.$param, $value);
+    }
+    return $statement;
+}
+
 ?>
